23443d9908cf2eb79991ff03486b58c5bb4dfe35
HansBaer78 <gebrauchte_vt@gmx.de>
2026-08-28 23:17:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Die App-Dateien werden beim ersten Aufruf gespeichert und danach
    immer aus dem Gerät geladen. Wetterabrufe gehen weiter ins Netz. */
 
-var CACHE = "logbuch-v8";
+var CACHE = "logbuch-v15";
 var DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./logo.png"];
 
 self.addEventListener("install", function(e){
